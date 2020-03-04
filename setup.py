89b791ad7e046e2c4e00cb8253e6b6954739026f
Alina Codzy <pazlvbanke@yandex.ru>
2020-03-04 13:51:45 +0300

    # version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='tvml',
-     version='0.1.6.1',
+     version='0.1.6.2',
      packages=['tvml'],
      author="Pazlvbanke",
      author_email="pazlvbanke@yandex.ru",
